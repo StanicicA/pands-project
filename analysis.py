53317d56a769a4786b5ac410d4227aaa5b5f5cff
StanicicA <g00411439@gmit.ie>
2022-04-26 00:47:09 +0100

    analysis.py

--- analysis.py
+++ analysis.py
@@ -12,22 +12,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Load Dataset into pandas dataframe
+#getting dataset info from a csv file
 data = pd.read_csv('iris.csv')
-
+print (data)
 #to get basic statistics about the data
 basicstat = (data.describe())
+print (basicstat)
 
 #to get basic info on the data
 info = data.info ()
-
+print (info)
 #to see number of samples for each species
-
 samples = data ['species'].value_counts()
+print (samples)
 
-# to display histogram 
+#visualisation/showing the graph using the function plot
+data.plot()
 
-data['sepallength'].hist()
 
+# to display the plot based on x and y 
+data.plot(kind = "scatter",x="sepallength",y="sepalwidth",color = "green")
+ 
+#to show the difference between the scatter based on species using seaborn function
+sns.FacetGrid(data,hue="species",size=5).map(plt.scatter,"sepallength","sepalwidth").add_legend()
 
+#if petal lenght/width is taken into account we can easly distinguish the difference in between versicolor and virginica from setosa
+sns.FacetGrid(data,hue="species",size=5).map(plt.scatter,"petallength","petalwidth").add_legend
+
+plt.show ()
 
